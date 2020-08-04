a23e9cd41b1a05392d005a3c05315ece86a6f1e7
Evgen <martunyke@gmail.com>
2020-08-04 20:18:41 +0300

    add dbcontext and employee controller

---
+++ DevoxTestTask/DataAccess/EmployeesActivityContext.cs
@@ -0,0 +1,26 @@
+﻿using DevoxTestTask.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevoxTestTask.DataAccess
+{
+    public class EmployeesActivityContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<ActivityType> ActivityTypes { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<EmployeeActivity> EmployeeActivity { get; set; }
+
+        public EmployeesActivityContext(DbContextOptions<EmployeesActivityContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        
+      
+    }
+}
